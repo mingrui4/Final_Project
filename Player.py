8b20e57d82793a7d2a154d407363b55b1126880a
songwei3 <songwei3@illinois.edu>
2018-04-29 23:38:31 -0500

    update player.py

--- Player.py
+++ Player.py
@@ -2,3 +2,24 @@
     Player.py
     Operation form players, including movement of the stone.
 '''
+
+class Human(object):
+    """
+    human player
+    """
+
+    def __init__(self, board, player):
+        self.board = board
+        self.player = player
+
+    def get_action(self, location):
+        try:
+            move = self.board.location_to_move(location)
+        except Exception as e:
+            move = -1
+        if move == -1 or move not in self.board.availables:
+            print("Invalid move")
+        return move
+
+    def __str__(self):
+        return "Human"
