6e98815988ff2a65a1fb5e212286af7b1abd2bc4
mingrui4 <mingrui4@illinois.edu>
2018-04-29 21:43:35 -0500

    Board.py

--- Board.py
+++ Board.py
@@ -2,3 +2,53 @@
     Board.py
     Create a board to play the game. Via data from Player.py and MCTS.py to put the stone.
 '''
+
+
+class Board(object):
+
+    def __init__(self, width=6, height=6, n_in_row=4):
+        self.width = width
+        self.height = height
+        self.states = {} # board states, key:move, value: player as piece type
+        self.n_in_row = n_in_row # need how many pieces in a row to win
+
+    def init_board(self):
+        if self.width < self.n_in_row :
+            raise Exception('board width can not less than %d' % self.n_in_row)
+
+        if self.height < self.n_in_row:
+            raise Exception('board height can not less than %d' % self.n_in_row)
+
+        self.availables = list(range(self.width * self.height)) # available moves
+
+        self.states = {} # key:move as location on the board, value:player as pieces type
+
+    def move_to_location(self, move):
+        """
+        3*3 board's moves like:
+        6 7 8
+        3 4 5
+        0 1 2
+        and move 5's location is (1,2)
+        """
+        h = move // self.width
+        w = move % self.width
+        return [h, w]
+
+    def location_to_move(self, location):
+        if(len(location) != 2):
+            move = -1
+            return move
+        h = location[0]
+        w = location[1]
+        move = h * self.width + w
+        if move not in self.availables:
+            move = -1
+        return move
+
+    def update(self, player, move):
+        self.states[move] = player
+        self.availables.remove(move)
+
+    def current_state(self):
+        return tuple((m, self.states[m]) for m in sorted(self.states)) # for hash
