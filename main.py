13a39d053ff0daf216a60d17fb1627bf9a67ce00
songwei3 <songwei3@illinois.edu>
2018-05-01 15:21:54 -0500

    update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ class Game:
 
 
 if __name__ == '__main__':
-    # init the board
+    # init the the game board
     game_board = Board(width=6, height=6, n_in_row=4)
     game = Game(game_board)
     game.start()
