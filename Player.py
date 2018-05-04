3ff4f2af851fad5657d41622caa357eb146f50a1
songwei3 <songwei3@illinois.edu>
2018-05-04 16:09:57 -0500

    update

--- Player.py
+++ Player.py
@@ -12,16 +12,20 @@ class Player(object):
         self.board = board
         self.player = player
 
-    def get_action(self):
+    def human_action(self):
+        """
+        ask player to input the position to put the stone
+        :return: integer
+        """
         try:
-            location = [int(n, 10) for n in input("Your move: ").split(",")]
-            move = self.board.location_to_move(location)
-        except Exception as e:
-            move = -1
-        if move == -1 or move not in self.board.blanks:
-            print("invalid move")
-            move = self.get_action()
-        return move
+            position = [int(n, 10) for n in input("Your move: ").split(",")]
+            put_stone = self.board.location_to_move(position)
+        except:
+            put_stone = -999
+        if put_stone == -999 or put_stone not in self.board.blanks:
+            print("This is a invalid move, please enter again!")
+            put_stone = self.human_action()
+        return put_stone
 
     def __str__(self):
         return "Human"
--- main.py
+++ main.py
@@ -1,58 +1,79 @@
 """
     main.py
-    Call other functions to implement the whole game.
-    Need to judge when to end the game.
+    Call functions in Board.py, Player.py, MCTS.py to implement the whole game.
+    Implemented the basic UI for the game.
 """
 
 from Player import Player
 from MCTS import MCTS
 from Board import Board
-from random import shuffle
 
 
 class Game:
-    def __init__(self, board, n_in_row = 4, time = 5, max_iteration = 1000):
+    def __init__(self, board, n_in_row=4, time=5, max_iteration=1000):
+        """
+        initialize the variables of the game
+        :param board:
+        :param n_in_row: default as 4
+        :param time: default as 5
+        :param max_iteration: default as 1000
+        """
         self.board = board
-        self.player = [1, 2]
+        self.player = [1, 2]  # display the human and ai players as player1 and player2
         self.n_in_row = n_in_row
         self.time = time
         self.max_iteration = max_iteration
 
     def init_player(self):
+        """
+        Ask the human player to choose whether to play first. Initialize and return the player turn.
+        :return: list
+        """
         choice = input("Do you want to play first ?(y/n) \n")
         if choice.lower() == 'y':
-            return [2, 1]
+            return [2, 1]    # human player is player2 and play first
         elif choice.lower() == 'n':
-            return [1, 2]
+            return [1, 2]    # AI play first
         else:
             print("Please input y or n ! \n")
-            result = self.init_player()
-            return result
+            play_turn = self.init_player()
+            return play_turn
 
     def init_game(self):
-        turn = self.init_player()
+        """
+        This the main function to control the process of the game. Call other functions to initialize the game and
+        play by turn until the game end situation is met.
+        :return:
+        """
+        # initialize the player and board
+        play_turn = self.init_player()
         self.board.init_board()
 
-        ai = MCTS(self.board, [1,2], self.n_in_row, self.time, self.max_iteration)
+        ai = MCTS(self.board, [1, 2], self.n_in_row, self.time, self.max_iteration)
         human = Player(self.board, 2)
         players = {}
-        players[1] = ai
-        players[2] = human
+        players[1] = ai  # store AI as value in player1
+        players[2] = human  # store human as value in player2
 
+        # implement the basic UI for the board and display the game
         self.draw_board(self.board, human, ai)
         while True:
-            current_p = turn.pop(0)
-            turn.append(current_p)
+            current_p = play_turn.pop(0)  # get the current player
+            play_turn.append(current_p)
             player_in_turn = players[current_p]
 
+            # get the actions of human and ai
             if str(player_in_turn) == 'Human':
                 print('Now is your turn :')
-                move = player_in_turn.get_action()
+                move = player_in_turn.human_action()
             else:
                 print('Now is AI turn :')
                 move = player_in_turn.action()
-            self.board.update(current_p, move)
-            self.draw_board(self.board, human, ai)
+
+            self.board.update(current_p, move)   # update the board
+            self.draw_board(self.board, human, ai)  # display the update
+
+            # judge whether to end the game after each step
             result, winner = self.game_end(ai)
             if result:
                 if winner != -1:
@@ -63,6 +84,11 @@ class Game:
                 break
 
     def game_end(self, ai):
+        """
+        Get the result of the game and the winner.
+        :param ai:
+        :return: Boolean, Integer
+        """
         has_win, winner = ai.winner(self.board)
         if has_win:
             return True, winner
@@ -72,6 +98,13 @@ class Game:
         return False, -1
 
     def draw_board(self, board, human, ai):
+        """
+        Implemented the basic display of the board and moves.
+        :param board:
+        :param human:
+        :param ai:
+        :return:
+        """
         width = board.width
         height = board.height
         print("Human Player", human.player, "with X \n")
@@ -82,31 +115,38 @@ class Game:
         for i in range(height - 1, -1, -1):
             print("{0:4d}".format(i), end='')
             for j in range(width):
-                loc = i * width + j
-                p = board.states.get(loc, -1)
-                if p == human.player:
+                position = i * width + j
+                player = board.states.get(position, -1)
+                if player == human.player:
                     print('X'.center(8), end='')
-                elif p == ai.player:
+                elif player == ai.player:
                     print('O'.center(8), end='')
                 else:
                     print('_'.center(8), end='')
             print('\r\n\r\n')
 
-def iinput():
-    width = input("Please input the width and height, the length need to larger than 4\n")
-    width = int(width)
-    if width <= 4:
-        print("Invalid input!")
-        width = iinput()
-        return width
-    return width
+
+def board_input():
+    """
+    Ask the human player to input the expected board size where width is equals to height.
+    :return: Integer
+    """
+    board_width = input("Please input the width/height of the board, the length need to be larger than 4\n")
+    board_width = int(board_width)
+    if board_width <= 4:
+        print("Invalid input! Please enter again!")
+        board_width = board_input()
+        return board_width
+    return board_width
 
 
 if __name__ == '__main__':
-    # init the the game board
-    width = iinput()
+
+    # init the the game board with width and height
+    width = board_input()
     height = width
-    # n_in_row = input('Please input the n_in_row')
+
+    # start the game
     game_board = Board(width, height, n_in_row=4)
     game = Game(game_board)
     game.init_game()
