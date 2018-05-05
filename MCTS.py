43a0c1a0b0d2478061805d5ddde5fe52bb447371
linnininini <nilin2@illinois.edu>
2018-05-05 23:34:58 +0800

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -2,7 +2,7 @@
     MCTS.py
     Core function. Use MCTS and RAVE to implement the AI.
     For point Ni, UCB = \frac{W_{i}}{N_{i}} + \sqrt{\frac{C \times lnN}{N_{i}}}
-    C = 2^0.5
+    C = 1.96
 """
 import numpy as np
 import time
